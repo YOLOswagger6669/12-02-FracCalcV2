53ffae444e8a1b194cafffb745b2ebccc67bdb3d
YOLOswagger6669 <samosley@gmail.com>
2014-12-02 19:48:28 -0700

    Update FracCalcV2.java

--- src/FracCalcV2.java
+++ src/FracCalcV2.java
@@ -11,3 +11,25 @@
 //     -- print frac1
 //   - divide frac1 by frac2
 //     -- print frac1
+public class FracCalcV2
+{
+  public static void main (String args[])
+  {
+    RationalNumber frac1= new RationalNumber();
+    RationalNumber frac2= new RationalNumber();
+    frac1.num=3;
+    frac1.denom=6;
+    frac2.num=2;
+    frac2.denom=8;
+    System.out.println(frac1.toString());
+    System.out.println(frac2.toString());
+    frac1.add(frac2);
+    System.out.println(frac1.toString());
+    frac1.subtract(frac2);
+    System.out.println(frac1.toString());
+    frac1.multiply(frac2);
+    System.out.println(frac1.toString());
+    frac1.divide(frac2);
+    System.out.println(frac1.toString());
+  }
+}
